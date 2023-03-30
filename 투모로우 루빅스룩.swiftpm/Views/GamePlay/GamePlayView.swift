609b7c4423bfc76da170fda2d9fc9279b2e50d1b
chamroro <chamsae@handong.ac.kr>
2023-03-30 12:53:45 +0900

    game play view ui with swipe effect

--- 투모로우 루빅스룩.swiftpm/Views/GamePlay/GamePlayView.swift
+++ 투모로우 루빅스룩.swiftpm/Views/GamePlay/GamePlayView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    var id: Int
+    var title: String
+    var color: Color
+}
+
+class Shirts: ObservableObject {
+    @Published var shirtsitems: [Item]
+    
+    let colors: [Color] = [.red, .orange, .blue, .teal, .mint, .green, .gray, .indigo]
+    
+    // dummy data
+    init() {
+        shirtsitems = []
+        for i in 0...6 {
+            let new = Item(id: i, title: "shirts \(i)", color: colors[i])
+            shirtsitems.append(new)
+        }
+    }
+}
+
+class Pants: ObservableObject {
+    @Published var pantsitems: [Item]
+    
+    let colors: [Color] = [.red, .orange, .blue]
+    
+    // dummy data
+    init() {
+        pantsitems = []
+        for i in 0...2 {
+            let new = Item(id: i, title: "Item \(i)", color: colors[i])
+            pantsitems.append(new)
+        }
+    }
+}
+
 struct GamePlayView: View {
     @Binding var isGameStarted: Bool
     
+    @StateObject var shirts = Shirts()
+    @StateObject var pants = Pants()
+    @State private var snappedShirtsItem = 0.0
+    @State private var draggingShirtsItem = 0.0
+    @State var activeShirtsIndex: Int = 0 //선택한 상의 인덱스
+    @State private var snappedPantsItem = 0.0
+    @State private var draggingPantsItem = 0.0
+    @State var activePantsIndex: Int = 0 //선택한 하의 인덱스
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -18,15 +63,81 @@ struct GamePlayView: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 VStack{
-                    Text("옆으로 스와이프 해서 루빅룩을 골라주세요!")
+                    Text("옆으로 스와이프 해서 ")
                         .font(.title3)
                         .fontWeight(.bold)
                         .padding(.top, 70.0)
-                    Image("룹기_어두운1")
-                        .resizable()
-                        .frame(width: 340.0, height: 550.0)
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.top, -50.0)
+                    Text("루빅룩을 골라주세요!")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding(.top, 0.0)
+                    
+                    Text("옷을 클릭하면 옷의 스토리를 볼 수 있어요")
+                        .padding(.init(top: 2, leading: 0, bottom: 25, trailing: 0))
+                        .font(.system(size: 15, weight: .semibold))
+                        .foregroundColor(Color(red: 0.229, green: 0.229, blue: 0.229))
+                    
+                    ZStack { //상의
+                        ForEach(shirts.shirtsitems) { shirtsitem in
+                            
+                            // article view
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 18)
+                                    .fill(shirtsitem.color)
+                                Text(shirtsitem.title)
+                                    .padding()
+                            }
+                            .frame(width: 340, height: 180)
+                            
+                            .scaleEffect(1.0 - abs(distanceforshirts(shirtsitem.id)) * 0.2 )
+                            .opacity(1.0 - abs(distanceforshirts(shirtsitem.id)) * 0.3 )
+                            .offset(x: myXOffsetforshirts(shirtsitem.id), y: 0)
+                            .zIndex(1.0 - abs(distanceforshirts(shirtsitem.id)) * 0.1)
+                            
+                            // Here is the modifier - on the item, not on the ForEach
+                            .onTapGesture {
+                                
+                                // withAnimation is necessary
+                                withAnimation {
+                                    
+                                    draggingShirtsItem = Double(shirtsitem.id)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.bottom, 25.0)
+                    .gesture(getDragGestureforshirts())
+                    
+                    ZStack { //하의
+                        ForEach(pants.pantsitems) { pantsitem in
+                            
+                            // article view
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 18)
+                                    .fill(pantsitem.color)
+                                Text(pantsitem.title)
+                                    .padding()
+                            }
+                            .frame(width: 200, height: 250)
+                            
+                            .scaleEffect(1.0 - abs(distanceforpants(pantsitem.id)) * 0.2 )
+                            .opacity(1.0 - abs(distanceforpants(pantsitem.id)) * 0.3 )
+                            .offset(x: myXOffsetforpants(pantsitem.id), y: 0)
+                            .zIndex(1.0 - abs(distanceforpants(pantsitem.id)) * 0.1)
+                            
+                            // Here is the modifier - on the item, not on the ForEach
+                            .onTapGesture {
+                                
+                                // withAnimation is necessary
+                                withAnimation {
+                                    draggingPantsItem = Double(pantsitem.id)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.bottom, 50.0)
+                    .gesture(getDragGestureforpants())
+                    
                     NavigationLink (destination:
                         GameResultView( isGameStarted: $isGameStarted)){
                         Text("제 픽은 이거에요!")
@@ -40,7 +151,65 @@ struct GamePlayView: View {
             }
         }
     }
+    private func getDragGestureforshirts() -> some Gesture {
+           DragGesture()
+               .onChanged { value in
+                   draggingShirtsItem = snappedShirtsItem + value.translation.width / 100
+               }
+               .onEnded { value in
+                   withAnimation {
+                       draggingShirtsItem = snappedShirtsItem + value.predictedEndTranslation.width / 100
+                       draggingShirtsItem = round(draggingShirtsItem).remainder(dividingBy: Double(shirts.shirtsitems.count))
+                       snappedShirtsItem = draggingShirtsItem
+                       
+                       //Get the active Item index
+                       self.activeShirtsIndex = shirts.shirtsitems.count + Int(draggingShirtsItem)
+                       if self.activeShirtsIndex > shirts.shirtsitems.count || Int(draggingShirtsItem) >= 0 {
+                           self.activeShirtsIndex = Int(draggingShirtsItem)
+                       }
+                   }
+               }
+       }
+    
+    func distanceforshirts(_ item: Int) -> Double {
+           return (draggingShirtsItem - Double(item)).remainder(dividingBy: Double(shirts.shirtsitems.count))
+       }
+       
+   func myXOffsetforshirts(_ item: Int) -> Double {
+       let angle = Double.pi * 2 / Double(shirts.shirtsitems.count) * distanceforshirts(item)
+       return sin(angle) * 200
+   }
+    
+    private func getDragGestureforpants() -> some Gesture {
+           DragGesture()
+               .onChanged { value in
+                   draggingPantsItem = snappedPantsItem + value.translation.width / 100
+               }
+               .onEnded { value in
+                   withAnimation {
+                       draggingPantsItem = snappedPantsItem + value.predictedEndTranslation.width / 100
+                       draggingPantsItem = round(draggingPantsItem).remainder(dividingBy: Double(pants.pantsitems.count))
+                       snappedPantsItem = draggingPantsItem
+                       
+                       //Get the active Item index
+                       self.activePantsIndex = pants.pantsitems.count + Int(draggingPantsItem)
+                       if self.activePantsIndex > pants.pantsitems.count || Int(draggingPantsItem) >= 0 {
+                           self.activePantsIndex = Int(draggingPantsItem)
+                       }
+                   }
+               }
+       }
+    
+    func distanceforpants(_ item: Int) -> Double {
+           return (draggingPantsItem - Double(item)).remainder(dividingBy: Double(pants.pantsitems.count))
+       }
+       
+   func myXOffsetforpants(_ item: Int) -> Double {
+       let angle = Double.pi * 2 / Double(pants.pantsitems.count) * distanceforpants(item)
+       return sin(angle) * 200
+   }
 }
+
 
 struct GamePlayView_Previews: PreviewProvider {
     static var previews: some View {
